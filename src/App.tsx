a071977e9989a3b21cf07a2d066a765eea96c6e9
Milltiades <gigakakulia2017@gmail.com>
2023-03-24 18:58:14 +0400

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,107 @@
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
 
-import { useForm } from 'react-hook-form'
-import styled from 'styled-components';
-import './App.css'
-
+import emailjs from "@emailjs/browser";
 function App() {
-  const {register, handleSubmit, formState: { errors }} = useForm();
-const onSubmit = () => {
-  console.log("submited")
-}
-  return (
-    <div className="App" >
-    <Form 
-    action="https://formspree.io/f/xpzejooa" method="POST"
-    onSubmit={handleSubmit(onSubmit)}
-    >
-      <Label htmlFor="">
-      Email
-      <Input type="mail" {...register("email", { required: true})} placeholder="example@gmail.com"/>
-      {errors.email?.type === 'required' && <P role="alert">Email is required</P>}
-      </Label>
-      <Label htmlFor="">
-      Full name
-        <Input type="text" {...register("fullName", { required: true})} placeholder="Name Surname"/>
-        {errors.fullName?.type === 'required' && <P role="alert">Full name is required</P>}
-        </Label>
+  const {} = useForm();
+ 
 
-        <button onClick={handleSubmit(onSubmit)}>Send</button>
-    </Form>
+  const form = useRef<any>();
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qyn8n7d",
+        "template_92xyabc",
+        form.current,
+        "FfhmiVaEjnViZtZ8C"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent")
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="App">
+    
+
+      <Form ref={form} onSubmit={sendEmail}>
+        <Label>Full name
+        <Input type="text" name="user_name"
+            placeholder="name surname"/>
+            
+        </Label>
+        <Label>Email
+        <Input type="email" name="user_email"
+            placeholder="example@gmail.com"/>
+            
+        </Label>
+        <MessageLabel>Message
+        <Textarea name="message" />
+        <SubmitInput type="submit" value="Send" />
+        </MessageLabel>
+      </Form>
     </div>
-  )
+  );
 }
 
 export default App;
+
+const Textarea = styled.textarea`
+  height: 100px;
+  width: 100%;
+`
+const MessageLabel = styled.label`
+  height: auto;
+`
 
 const P = styled.p`
   font-size: 15px;
   color: red;
   margin: 0 5px;
-`
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-`
+  border: 1px solid black;
+  padding: 32px;
+ 
+`;
 
 const Label = styled.label`
-display: flex;
-flex-direction: column;
-align-items: flex-start;
-
-position: relative;
-height: 100px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: relative;
+  height: 65px;
+`;
 
 const Input = styled.input`
   width: 100%;
   height: 25px;
-  padding: 5px ;
+  padding: 5px;
   border-radius: 5px;
-  
+`;
+const SubmitInput = styled.input`
+  width: 40%;
+  height: 25px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 25px;
+  height: 50px;
 `
